41a7739a74425bcb08c1cf293b8d6e852c63e22f
deepa-kumari <deeparawat2008@gmail.com>
2013-06-18 23:57:52 +0530

    TestProgram

---
+++ MyConsoleApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = ("Hello World!");
+            Console.WriteLine(myString);
+            Console.ReadLine();
+        }
+    }
+}
