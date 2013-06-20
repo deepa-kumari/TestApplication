1ee848c50dd9e563a514c944cb708654d7920e16
deepa-kumari <deeparawat2008@gmail.com>
2013-06-20 13:42:21 +0530

    DateTimeProgram

---
+++ DateAndTime/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           DateTime myValue = DateTime.Now;
+           //Console.WriteLine(myValue.ToString());
+           //Console.WriteLine(myValue.ToShortDateString());
+           //Console.WriteLine(myValue.ToShortTimeString());
+           //Console.WriteLine(myValue.AddDays(3).ToShortDateString());
+           //Console.WriteLine(myValue.AddDays(-3).ToShortDateString());
+           //Console.WriteLine(myValue.AddHours(3).ToShortTimeString());
+          // Console.WriteLine(myValue.AddHours(-3).ToShortTimeString());
+           //Console.WriteLine(myValue.Month.ToString());
+            DateTime myBirthDate = DateTime.Parse("1987/01/07");
+            //DateTime myBirthDate = new DateTime(1987, 01, 07);
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthDate);
+            Console.WriteLine(myAge.TotalDays.ToString());
+          
+            Console.ReadLine();
+        }
+    }
+}
